02d2cf98df26fccb29bf70b8433abd3fbad8a6d7
Alc2110 <38523363+Alc2110@users.noreply.github.com>
2021-04-25 18:43:33 +1000

    implement about box

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 const electron = require('electron');
 const url = require('url');
 const path = require('path');
+const { remote } = require('electron');
 
 const { app, BrowserWindow, Menu } = electron;
 
 let mainWindow;
+let aboutBox;
 
 // Listen for app ready
 app.on('ready', function(){
@@ -38,13 +40,51 @@ app.on('ready', function(){
 const mainMenuTemplate = [
     {
         label: 'File',
-        submenu: [
+        submenu:
+        [
             {
                 label: 'Exit',
                 accelerator: process.platform == 'darwin' ? 'Command+Q' : 
                 'Ctrl+Q',
-                click(){
+                click() {
                     app.quit();
+                }
+            }
+        ]
+    },
+    {
+        label: 'Help',
+        submenu:
+        [
+            {
+                label: 'About',
+                click() {
+                    // show about box
+                    // create window
+                    aboutBox = new BrowserWindow({
+                        width: 400,
+                        height: 325,
+                        title: 'About FitnessCalculator',
+                        parent: mainWindow,
+                        show: true,
+                        modal: true,
+                        webPreferences: {
+                            nodeIntegration: true,
+                            contextIsolation: false,
+                            enableRemoteModule: true
+                        }
+                    });
+                    aboutBox.setMenuBarVisibility(false);
+                    aboutBox.resizable = false;
+                    aboutBox.minimizable = false;
+                    // load HTML
+                    aboutBox.loadURL(url.format({
+                        pathname: path.join(__dirname, '/about_box/about.html'),
+                        protocol: 'file:',
+                        slashes: true
+                    }));
+                    // garbage collection
+                    aboutBox.on('close', () => {aboutBox=null;})
                 }
             }
         ]
@@ -52,15 +92,16 @@ const mainMenuTemplate = [
 ];
 
 // if on Mac, add empty object to menu (by default the first menu item is "Electron")
-if (process.platform == 'darwin'){
+if (process.platform == 'darwin') {
     mainMenuTemplate.unshift({});
 }
 
 // add developer tools item if not in production
-if (process.env.NODE_ENV !== 'production'){
+if (process.env.NODE_ENV !== 'production') {
     mainMenuTemplate.push({
         label: 'Developer Tools',
-        submenu:[
+        submenu:
+        [
             {
                 label: 'Toggle DevTools',
                 accelerator: process.platform == 'darwin' ? 'Command+I' :
